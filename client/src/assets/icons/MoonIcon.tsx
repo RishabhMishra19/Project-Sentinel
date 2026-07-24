ef97c053d4cb591f22592bee85342483a70689f3
Rishabh Mishra <rishabhm@sprinto.com>
2026-07-24 16:24:02 +0530

    Theme icons

---
+++ client/src/assets/icons/MoonIcon.tsx
@@ -0,0 +1,19 @@
+type IconProps = {
+  className?: string
+}
+
+export const MoonIcon = ({ className }: IconProps) => (
+  <svg
+    className={className}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="1.5"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    aria-hidden
+  >
+    <path d="M21 14.5A8.5 8.5 0 0 1 9.5 3 7 7 0 1 0 21 14.5z" />
+  </svg>
+)
---
+++ client/src/assets/icons/index.ts
@@ -0,0 +1,4 @@
+export { SunIcon } from './SunIcon'
+export { MoonIcon } from './MoonIcon'
+export { OverviewIcon } from './OverviewIcon'
+export { SettingsIcon } from './SettingsIcon'
