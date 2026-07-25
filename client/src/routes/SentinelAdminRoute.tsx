4a31230edb2e4e5b24f642202c220dcdf5381d13
Rishabh Mishra <rishabhm@sprinto.com>
2026-07-25 19:20:13 +0530

    Show UnauthorizedPage when non-admins hit Sentinel admin routes.

--- client/src/routes/SentinelAdminRoute.tsx
+++ client/src/routes/SentinelAdminRoute.tsx
@@ -1,21 +1,13 @@
-import { Navigate, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import { UnauthorizedPage } from '../features/auth/pages/UnauthorizedPage'
 import { useAppSelector } from '../redux/hooks'
-import { ROUTES } from './paths'
 
-/** Requires Sentinel admin after session is ready; redirects others to Profile. */
+/** Requires Sentinel admin; shows UnauthorizedPage otherwise. */
 export function SentinelAdminRoute() {
-  const user = useAppSelector((state) => state.session.user)
-
-  if (!user) {
-    return (
-      <div className="flex flex-1 items-center justify-center text-muted">
-        Loading account…
-      </div>
-    )
-  }
+  const user = useAppSelector((state) => state.session.user)!
 
   if (!user.sentinelAdmin) {
-    return <Navigate to={ROUTES.PROFILE} replace />
+    return <UnauthorizedPage />
   }
 
   return <Outlet />
